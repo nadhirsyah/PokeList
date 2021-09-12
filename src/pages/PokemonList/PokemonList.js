ca7132c8b5f293a5293a73904b7ac8a528c758ba
nadhirsyah <nadhirsyah@gmail.com>
2021-09-12 22:42:49 +0700

    add total owned pokemon

--- src/pages/PokemonList/PokemonList.js
+++ src/pages/PokemonList/PokemonList.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import styled from '@emotion/styled'
 import { useQuery } from '@apollo/client';
 import { GET_POKEMONS } from '../../graphql/GetPokemon'
 import { PokemonListComponent } from '../../components/PokemonListComponent/PokemonListComponent';
+import { PokemonContext } from '../../context/PokemonContext';
+export const Title = styled.h2`
+    text-align: center;
+`;
 export function PokemonList() {
-    
+    const {myPokemons, setMyPokemons} = useContext(PokemonContext)
     const {data:{ pokemons = []} = {}} = useQuery(GET_POKEMONS, {
         variables: {
             first: 151
@@ -12,7 +17,10 @@ export function PokemonList() {
 
     if (pokemons){
         return(
+            <>
+            <Title>Total Owned Pokemon: {myPokemons.length}</Title>
             <PokemonListComponent pokemons={pokemons}/>
+            </>
 
         )
     } else {
